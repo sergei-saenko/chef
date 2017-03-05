6b83b77cde9336e7be8a07b88c3ff20d097b8749
Sergei Saenko <sergei.saenko@hotmail.com>
2017-03-05 21:59:25 +0200

    motd recipe added

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,8 +3,18 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+#
+
+
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elsif node['platform_family'] == "debian"
+	package = "apache2"
+end
+
+
 package 'apache2' do
-	package_name 'httpd'
+	package_name package
 	action :install
 end
 
@@ -13,3 +23,5 @@ service 'apache2' do
 	service_name 'httpd'
 	action [:start, :enable]
 end
+
+#include_recipe 'apache::website'

5df701ed8dbed2e116aaa227358920fa9d252a33
sergei-saenko <sergei.saenko@hotmail.com>
2017-03-12 21:59:27 +0200

    apache recipe was modified to recognize diferent pplatforms

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,3 +3,15 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'apache' do
+  case node[:platform]
+  when 'redhat', 'centos'
+    package_name 'httpd'
+	version '2.4.6-45'
+  when 'ubuntu', 'debian'
+    package_name 'apache2'
+	version '2.4.7'
+  end
+end
+

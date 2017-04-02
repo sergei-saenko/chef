181533a991f300002835e2a4ce3de9e637ef0785
sergei-saenko <sergei.saenko@hotmail.com>
2017-04-02 19:33:04 +0300

    recipe were semplified

--- cookbooks/mariadb/recipes/phpmyadmin.rb
+++ cookbooks/mariadb/recipes/phpmyadmin.rb
@@ -5,11 +5,8 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 package 'phpmyadmin' do
-  case node[:platform]
-  when 'redhat', 'centos'
-    package_name 'phpmyadmin'
-	  when 'ubuntu', 'debian'
-    package_name 'phpmyadmin'
-	  end
+end
+
+package 'php' do
 end
 

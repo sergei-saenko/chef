188ab1e4a33714e45deaf74e5739bf7b41373d72
sergei-saenko <sergei.saenko@hotmail.com>
2017-04-02 19:28:15 +0300

    all strings were replaced with include statements

--- cookbooks/mariadb/recipes/default.rb
+++ cookbooks/mariadb/recipes/default.rb
@@ -3,25 +3,6 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-package 'mariadb' do
-  case node[:platform]
-  when 'redhat', 'centos'
-    package_name 'mariadb-server'
-	  when 'ubuntu', 'debian'
-    package_name 'mariadb-server'
-	  end
-end
-
-service 'mariadb-service' do
-  case node[:platform]
-  when 'redhat', 'centos'
-    service_name 'mariadb'
-    action [:enable, :start]
-	when 'ubuntu', 'debian'
-    service_name 'mariadb'
-    action [:enable, :start]
-	end
-end
-
 
 include_recipe 'mariadb::base'
+include_recipe 'mariadb::phpmyadmin'

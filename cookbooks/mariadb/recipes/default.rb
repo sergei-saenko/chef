dc89ab9f4d38ed4b8f676b345c627382c7bfd5e9
sergei-saenko <sergei.saenko@hotmail.com>
2017-04-02 18:44:18 +0300

    mariadb default recipe was written

--- cookbooks/mariadb/recipes/default.rb
+++ cookbooks/mariadb/recipes/default.rb
@@ -3,3 +3,22 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+package 'mariadb' do
+  case node[:platform]
+  when 'redhat', 'centos'
+    package_name 'mariadb-server'
+	  when 'ubuntu', 'debian'
+    package_name 'mariadb-server'
+	  end
+end
+
+service 'mariadb-service' do
+  case node[:platform]
+  when 'redhat', 'centos'
+    service_name 'mariadb'
+    action [:enable, :start]
+	when 'ubuntu', 'debian'
+    service_name 'mariadb'
+    action [:enable, :start]
+	end
+end
